9008b169ffaec3d3c5715334a1b8333400135c85
foxhsiao <fox@moneyball.io>
2016-09-18 01:29:05 +0800

    Add article index blade

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -10,6 +11,8 @@ class ArticlesController extends Controller
 {
     public function index()
     {
+        $articles = Article::all();
 
+        return view('articles.index', compact('articles'));
     }
 }
