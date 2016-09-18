157c61b1beedddadfe76e4c85dc9ce59c0d1b221
foxhsiao <fox@moneyball.io>
2016-09-18 08:34:59 +0800

    Add find article

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,7 +3,7 @@
 <h1>Articles</h1>
 @foreach($articles as $article)
     <article>
-        <h2>{{ $article->title }}</h2>
+        <h2><a href="{{ url('articles', $article->id) }}">{{ $article->title }}</a></h2>
         <div class="body">{{ $article->body }}</div>
     </article>
 @endforeach
