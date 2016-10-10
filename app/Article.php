ad58fdc77634c85b435015f39f5abb9d36084da8
foxhsiao <fox@moneyball.io>
2016-10-10 17:03:01 +0800

    Add article tag migrate

--- app/Article.php
+++ app/Article.php
@@ -41,4 +41,9 @@ class Article extends Model
     {
         return $this->belongsTo('App\User');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 }
